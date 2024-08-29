caebf6ed80c05f2fc11f7ddf5b993e415f7388d3
lunge.keswa@ignitiongroup.co.za <#@Sm88th.1>
2024-08-29 23:42:39 +0200

    images

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image, Dimensions } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
-export default function App() {
+
+const imageTwo = require('./assets/sliderImages/1.png');
+const imageThree = require('./assets/sliderImages/2.png');
+const imageFour = require('./assets/sliderImages/3.png');
+
+const win = Dimensions.get('window');
+
+export default function MyPager() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PagerView style={styles.container} initialPage={0}>
+        <View style={styles.page} key="1">
+          <Image 
+            source={imageTwo} 
+            style={styles.image} 
+          />
+          <Text>Browse</Text>
+        </View>
+        <View style={styles.page} key="2">
+          <Image 
+            source={imageThree} 
+            style={styles.image} />
+             <Text>Explore</Text>
+        </View>
+        <View style={styles.page} key="3">
+          <Image 
+            source={imageFour} 
+            style={styles.image} 
+          />
+          <Text>Hello</Text>
+        </View>
+      </PagerView>
     </View>
   );
 }
@@ -13,8 +40,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft: 20,
+    marginRight: 20
   },
+  image: {
+     width: win.width,
+     height: win.height,
+    // height: Dimensions.get('window').width,
+    // height: Dimensions.get('window').width,
+  }
 });
